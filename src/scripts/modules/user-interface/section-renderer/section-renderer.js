ca55fb32f3d3a119d32f6c3cef44aa41e4b315fa
Serhii <avatarcapa@gmail.com>
2026-01-31 01:06:33 +0100

    fix: storing sectionID when data and before typing animation is complete (so section restored if animation interrupted)

--- src/scripts/modules/user-interface/section-renderer/section-renderer.js
+++ src/scripts/modules/user-interface/section-renderer/section-renderer.js
@@ -103,12 +103,13 @@ class SectionRenderer {
         block: 'end'
       });
 
+      this.stateManager.addRevealedSection(sectionId);
+
       await this.sectionAnimator.animateSection(sectionElement, sectionContent);
 
       this._renderMetaItems(sectionElement, sectionId, sectionMetadata, profileData, role);
       this._revealMetaItems(sectionElement);
 
-      this.stateManager.addRevealedSection(sectionId);
       trackSectionView(sectionId);
 
       this._updateActionPrompt();
